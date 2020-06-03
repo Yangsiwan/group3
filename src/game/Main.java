dc4440595f7d7b51061376c55d2b10601b06703a
Yangsiwan <21900429@handong.edu>
2020-06-03 12:26:22 +0900

    First game

--- src/game/Main.java
+++ src/game/Main.java
@@ -3,7 +3,6 @@ package game;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 	  Character ch = new Character("Minsu",50);
       Chunmaji chun = new Chunmaji();
       chun.show();
