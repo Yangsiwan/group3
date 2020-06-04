db531c9be50b67687700e8ce8f1e6178e4c4dd7d
MasicTurtle <61458692+HyeokyYun@users.noreply.github.com>
2020-06-04 13:13:26 +0900

    Update Match.java

--- main/Match.java
+++ main/Match.java
@@ -11,7 +11,7 @@ public class Match {
 	
 	//Game 1 method
 	boolean Game1() {
-		System.out.println("\n[Game 1]\n");
+		System.out.println("\n[Match 1]\n");
 		System.out.println("Vision vs Bethel");
 		System.out.println("----------------------------");
 		System.out.println("| Status | Vision | Bethel |");
@@ -45,27 +45,27 @@ public class Match {
 		if(team1 > team2) {
 			check = "Vision";
 			try {
-				System.out.println("Match has begun!");
-				Thread.sleep(1000);
-				System.out.println("경기초반부터 Vision이 몰아붙이고 있습니다.");
-				Thread.sleep(1000);
-				System.out.println("오늘따라 Vision선수들 컨디션이 아주 좋아보여요.");
-				Thread.sleep(1000);
-				System.out.println("아 말씀드리는 순간 기회를 만들었어요!");
-				Thread.sleep(1000);
-				System.out.println("빈공간... 일대일 찬스입니다!");
-				Thread.sleep(1000);
-				System.out.println("슛!!!");
-				Thread.sleep(1000);
-				System.out.println("골!!! 골이에요!!! Vision이 득점에 성공합니다!");
-				Thread.sleep(1000);
-				System.out.println("Matching....");
-				Thread.sleep(1000);
+				System.out.println("\nMatch has begun!");
+				Thread.sleep(800);
+				System.out.println("Vision has been pushing us since the beginning of the game.");
+				Thread.sleep(800);
+				System.out.println("Vision players look very good today.");
+				Thread.sleep(800);
+				System.out.println("Oh, they have made a chance!");
+				Thread.sleep(800);
+				System.out.println("Free space... one-on-one chance!");
+				Thread.sleep(800);
+				System.out.println("shoot!!!");
+				Thread.sleep(800);
+				System.out.println("Goal!!! Goal!!! Vision scores!");
+				Thread.sleep(800);
+				System.out.println("Matching");
+				Thread.sleep(800);
 				System.out.println("....");
-				Thread.sleep(1000);
+				Thread.sleep(700);
 				System.out.println("....");
-				Thread.sleep(1000);
-				System.out.println("경기 종료됩니다! 이변은 일어나지 않았습니다.");
+				Thread.sleep(700);
+				System.out.println("The game is over! Nothing unusual happened.");
 				Thread.sleep(500);
 				System.out.println("Vision: " + team1 + " Bethel: " + team2);
 				Thread.sleep(1000);
@@ -73,38 +73,38 @@ public class Match {
 			catch(InterruptedException e) {
 				return true ;
 			}
-			System.out.println("결국 Vision이 승리를 가져가네요.\n.\n.\n.");
+			System.out.println("Vision finally wins.\n.\n.\n.");
 		}
 		
 		//when team2 wins.
 		if (team1 < team2) {
 			check = "Bethel";
 			try {
-				System.out.println("Match has begun!");
-				Thread.sleep(1000);
-				System.out.println("경기초반부터 Vision이 몰아붙이고 있습니다.");
-				Thread.sleep(1000);
-				System.out.println("양팀의 전력차이가 꽤 있거든요. Bethel에게는 쉽지 않은 경기입니다.");
-				Thread.sleep(1000);
-				System.out.println("아 말씀드리는 순간 기회를 만들었어요!");
-				Thread.sleep(1000);
-				System.out.println("빈공간... 일대일 찬스입니다!");
-				Thread.sleep(1000);
-				System.out.println("슛!!!");
-				Thread.sleep(1000);
-				System.out.println("골!!! 골이에요!!! Bethel 득점에 성공합니다!");
-				Thread.sleep(1000);
-				System.out.println("이게 풋살이거든요. 공은 둥글기 때문에 끝까지 가봐야 아는 겁니다.");
-				Thread.sleep(1000);
-				System.out.println("Bethel 선수들 오늘따라 날라다니네요!");
-				Thread.sleep(1000);
-				System.out.println("Matching....");
-				Thread.sleep(1000);
+				System.out.println("\nMatch has begun!");
+				Thread.sleep(800);
+				System.out.println("Vision has been pushing us since the beginning of the game.");
+				Thread.sleep(800);
+				System.out.println("There's a big difference between the two teams. It's not an easy game for Bethel.");
+				Thread.sleep(800);
+				System.out.println("Oh, they have made a chance!");
+				Thread.sleep(800);
+				System.out.println("Free space... one-on-one chance!\r\n");
+				Thread.sleep(800);
+				System.out.println("shoot!!!");
+				Thread.sleep(800);
+				System.out.println("Goal!!! Goal!!! Bethel have scored!");
+				Thread.sleep(800);
+				System.out.println("This is futsal. The ball is round, so you have to go all the way to find out.");
+				Thread.sleep(800);
+				System.out.println("Bethel players are flying around today!");
+				Thread.sleep(800);
+				System.out.println("Matching");
+				Thread.sleep(800);
 				System.out.println("....");
-				Thread.sleep(1000);
+				Thread.sleep(800);
 				System.out.println("....");
-				Thread.sleep(1000);
-				System.out.println("경기 종료됩니다! 기적이 일어났습니다!");
+				Thread.sleep(800);
+				System.out.println("The game is over! A miracle has happened!");
 				Thread.sleep(500);
 				System.out.println("Vision: " + team1 + " Bethel: " + team2);
 				Thread.sleep(1000);
@@ -112,7 +112,7 @@ public class Match {
 			catch(InterruptedException e) {
 				return true ;
 			}
-			System.out.println("Bethel에서 승리를 가져가네요.\n.\n.\n.");
+			System.out.println("Bethel is taking the victory.\n.\n.\n.");
 		}
 		
 		//If users' input and team's name are same.
@@ -127,7 +127,7 @@ public class Match {
 	}
 		
 	boolean Game2() {
-		System.out.println("\n[Game 2]\n");
+		System.out.println("\n[Match 2]\n");
 		System.out.println("Lothem vs Creation");
 		System.out.println("----------------------------");
 		System.out.println("| Status | Lothem |Creation|");
@@ -156,27 +156,27 @@ public class Match {
 		if(team1 > team2) {
 			check = "Lothem";
 			try {
-				System.out.println("Match has begun!");
-				Thread.sleep(1000);
-				System.out.println("양팀 비슷한 경기력을 가지고 있습니다. 누가이길지 전혀 모르겠습니다.");
-				Thread.sleep(1000);
-				System.out.println("양팀모두 최선을 다해서 뛰고 있습니다.");
-				Thread.sleep(1000);
-				System.out.println("아 말씀드리는 순간 코너킥 준비하고 있습니다.");
-				Thread.sleep(1000);
-				System.out.println("올렸어요.");
-				Thread.sleep(1000);
-				System.out.println("헤딩!!!");
-				Thread.sleep(1000);
-				System.out.println("골!!! 골이에요!!! Lothem 득점에 성공합니다!");
-				Thread.sleep(1000);
-				System.out.println("Matching....");
-				Thread.sleep(1000);
+				System.out.println("\nMatch has begun!");
+				Thread.sleep(800);
+				System.out.println("Both teams have similar performance. I have no idea who will win.");
+				Thread.sleep(800);
+				System.out.println("Both teams are doing their best.");
+				Thread.sleep(800);
+				System.out.println("They are getting ready for corner kick.");
+				Thread.sleep(800);
+				System.out.println("Cross.");
+				Thread.sleep(800);
+				System.out.println("Header!!!");
+				Thread.sleep(800);
+				System.out.println("Goal!!! Goal!!! Lothem scores successfully!");
+				Thread.sleep(800);
+				System.out.println("Matching");
+				Thread.sleep(800);
 				System.out.println("....");
-				Thread.sleep(1000);
+				Thread.sleep(800);
 				System.out.println("....");
-				Thread.sleep(1000);
-				System.out.println("경기 종료됩니다! 양팀모두 최고의 경기력이었습니다!");
+				Thread.sleep(800);
+				System.out.println("Match is over. It was the best game ever.");
 				Thread.sleep(500);
 				System.out.println("Lothem: " + team1 + " Creation: " + team2);
 				Thread.sleep(1000);
@@ -184,33 +184,33 @@ public class Match {
 			catch(InterruptedException e) {
 				return true ;
 			}
-			System.out.println("결국 Lothem이 승리를 가져가네요.\n.\n.\n.");
+			System.out.println("Lothem finally wins.\n.\n.\n.");
 		}
 		
 		if (team1 < team2) {
 			check = "Creation";
 			try {
-				System.out.println("Match has begun!");
-				Thread.sleep(1000);
-				System.out.println("양팀 비슷한 경기력을 가지고 있습니다. 누가이길지 전혀 모르겠습니다.");
-				Thread.sleep(1000);
-				System.out.println("양팀모두 최선을 다해서 뛰고 있습니다.");
-				Thread.sleep(1000);
-				System.out.println("아 말씀드리는 순간 코너킥 준비하고 있습니다.");
-				Thread.sleep(1000);
-				System.out.println("올렸어요.");
-				Thread.sleep(1000);
-				System.out.println("헤딩!!!");
-				Thread.sleep(1000);
-				System.out.println("골!!! 골이에요!!! Lothem 득점에 성공합니다!");
-				Thread.sleep(1000);
-				System.out.println("Matching....");
-				Thread.sleep(1000);
+				System.out.println("\nMatch has begun!");
+				Thread.sleep(800);
+				System.out.println("Both teams have similar performance. I have no idea who will win.");
+				Thread.sleep(800);
+				System.out.println("Both teams are doing their best.");
+				Thread.sleep(800);
+				System.out.println("They are getting ready for corner kick.");
+				Thread.sleep(800);
+				System.out.println("Cross.");
+				Thread.sleep(800);
+				System.out.println("Header!!!");
+				Thread.sleep(800);
+				System.out.println("Goal!!! Goal!!! Creation scores successfully!");
+				Thread.sleep(800);
+				System.out.println("Matching");
+				Thread.sleep(800);
 				System.out.println("....");
-				Thread.sleep(1000);
+				Thread.sleep(800);
 				System.out.println("....");
-				Thread.sleep(1000);
-				System.out.println("경기 종료됩니다! 양팀모두 최고의 경기력이었습니다!");
+				Thread.sleep(800);
+				System.out.println("Match is over. It was the best game ever.");
 				Thread.sleep(500);
 				System.out.println("Lothem: " + team1 + " Creation: " + team2);
 				Thread.sleep(1000);
@@ -218,7 +218,7 @@ public class Match {
 			catch(InterruptedException e) {
 				return true ;
 			}
-			System.out.println("Creaion 에서 승리를 가져가네요.\n.\n.\n.");
+			System.out.println("Creaion finally wins.\n.\n.\n.");
 		}
 		
 		if(predict.equals(check)) {
@@ -232,7 +232,7 @@ public class Match {
 	}
 	
 	boolean Game3() {
-		System.out.println("\n[Game 3]\n");
+		System.out.println("\n[Match 3]\n");
 		System.out.println("Grace vs International");
 		System.out.println("----------------------------");
 		System.out.println("| Status | Grace  |  Int.  |");
@@ -261,69 +261,73 @@ public class Match {
 		if(team1 > team2) {
 			check = "Grace";
 			try {
-				System.out.println("Match has begun!");
-				Thread.sleep(1000);
-				System.out.println("양팀 비슷한 경기력을 가지고 있습니다. 누가이길지 전혀 모르겠습니다.");
-				Thread.sleep(1000);
-				System.out.println("양팀모두 최선을 다해서 뛰고 있습니다.");
-				Thread.sleep(1000);
-				System.out.println("아 말씀드리는 순간 코너킥 준비하고 있습니다.");
-				Thread.sleep(1000);
-				System.out.println("올렸어요.");
-				Thread.sleep(1000);
-				System.out.println("헤딩!!!");
-				Thread.sleep(1000);
-				System.out.println("골!!! 골이에요!!! Grace 득점에 성공합니다!");
-				Thread.sleep(1000);
-				System.out.println("Matching....");
-				Thread.sleep(1000);
+				System.out.println("\nMatch has begun!");
+				Thread.sleep(800);
+				System.out.println("Both teams have similar performance. I have no idea who will win.");
+				Thread.sleep(800);
+				System.out.println("Both teams are doing their best.");
+				Thread.sleep(800);
+				System.out.println("They are getting ready for corner kick.");
+				Thread.sleep(800);
+				System.out.println("Oops, there is an foul.");
+				Thread.sleep(800);
+				System.out.println("They Grace is preparing penalty kick.");
+				Thread.sleep(800);
+				System.out.println("Shoot!");
+				Thread.sleep(800);
+				System.out.println("Goal!!! Goal!!! Grace scores successfully!");
+				Thread.sleep(800);
+				System.out.println("Matching");
+				Thread.sleep(800);
 				System.out.println("....");
-				Thread.sleep(1000);
+				Thread.sleep(800);
 				System.out.println("....");
-				Thread.sleep(1000);
-				System.out.println("경기 종료됩니다! 양팀모두 최고의 경기력이었습니다!");
+				Thread.sleep(800);
+				System.out.println("Match is over. It was the best game ever.");
 				Thread.sleep(500);
-				System.out.println("Grace: " + team1 + " International: " + team2);
+				System.out.println("Lothem: " + team1 + " Creation: " + team2);
 				Thread.sleep(1000);
 			}
 			catch(InterruptedException e) {
 				return true ;
 			}
-			System.out.println("결국 Grace이 승리를 가져가네요.\n.\n.\n.");
+			System.out.println("Grace finally wins.\n.\n.\n.");
 		}
 		
 		if (team1 < team2) {
 			check = "International";
 			try {
-				System.out.println("Match has begun!");
-				Thread.sleep(1000);
-				System.out.println("양팀 비슷한 경기력을 가지고 있습니다. 누가이길지 전혀 모르겠습니다.");
-				Thread.sleep(1000);
-				System.out.println("양팀모두 최선을 다해서 뛰고 있습니다.");
-				Thread.sleep(1000);
-				System.out.println("아 말씀드리는 순간 코너킥 준비하고 있습니다.");
-				Thread.sleep(1000);
-				System.out.println("올렸어요.");
-				Thread.sleep(1000);
-				System.out.println("헤딩!!!");
-				Thread.sleep(1000);
-				System.out.println("골!!! 골이에요!!! International 득점에 성공합니다!");
-				Thread.sleep(1000);
-				System.out.println("Matching....");
-				Thread.sleep(1000);
+				System.out.println("\nMatch has begun!");
+				Thread.sleep(800);
+				System.out.println("Both teams have similar performance. I have no idea who will win.");
+				Thread.sleep(800);
+				System.out.println("Both teams are doing their best.");
+				Thread.sleep(800);
+				System.out.println("They are getting ready for corner kick.");
+				Thread.sleep(800);
+				System.out.println("Oops, there is an foul.");
+				Thread.sleep(800);
+				System.out.println("They International is preparing penalty kick.");
+				Thread.sleep(800);
+				System.out.println("Shoot!");
+				Thread.sleep(800);
+				System.out.println("Goal!!! Goal!!! International scores successfully!");
+				Thread.sleep(800);
+				System.out.println("Matching");
+				Thread.sleep(800);
 				System.out.println("....");
-				Thread.sleep(1000);
+				Thread.sleep(800);
 				System.out.println("....");
-				Thread.sleep(1000);
-				System.out.println("경기 종료됩니다! 양팀모두 최고의 경기력이었습니다!");
+				Thread.sleep(800);
+				System.out.println("Match is over. It was the best game ever.");
 				Thread.sleep(500);
-				System.out.println("Grace: " + team1 + " International: " + team2);
+				System.out.println("Lothem: " + team1 + " Creation: " + team2);
 				Thread.sleep(1000);
 			}
 			catch(InterruptedException e) {
 				return true ;
 			}
-			System.out.println("International 에서 승리를 가져가네요.\n.\n.\n.");
+			System.out.println("International finally wins.\n.\n.\n.");
 		}
 		
 		if(predict.equals(check)) {
