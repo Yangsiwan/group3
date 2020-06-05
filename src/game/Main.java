36b3c6f5eea72c7431025d17bbe67b476fb79e6c
ad7051 <63580888+ad7051@users.noreply.github.com>
2020-06-05 19:17:45 +0900

    Update Main.java

--- src/game/Main.java
+++ src/game/Main.java
@@ -3,7 +3,7 @@ import java.util.*;
 class Main {
 static String name;
   static int hp = 50;
-	static int [] clear= new int [10];
+	static boolean [] clear= new boolean [11];
 	static String [] Sname= {"GLC","Hyoam Chaple","Cheonmaji","Store","Alpha Stationery","Student Union","Hiddink Field","Oseok Hall","Newton Hall","Hyundong Hall"};
 	 public static void menu() {
 		   System.out.println("===================[MAP]================");
@@ -27,40 +27,62 @@ static String name;
 	public static void main(String[] args) { 
 		 int check=1;
 		 
-		 int menu;
-		 try (Scanner scn = new Scanner(System.in)) {
+		 int menu=0;
+		 Scanner scn = new Scanner(System.in);
 			System.out.println("========================================");
 			System.out.println("Welcome to Your Hanst!!");
 			 System.out.printf("Please type your name: ");
 			 while(check==1) {
 				 name=scn.nextLine();
-				 System.out.println("Your name is \""+name+"\" right? (0:Yes 1:No)");
-				 check=scn.nextInt();
-				 if(check==0) {
+				 while(check==1) {
+				 try {
+					 System.out.println("Your name is \""+name+"\" right? (0:Yes 1:No)");
+					 check=scn.nextInt();
+					 scn.nextLine();
+					 if(check!=1&&check!=0) throw new MismatchException ();
+					 break;
+				 	} catch(InputMismatchException e){
+					 	System.out.println("You Put the Wrong Type!! please re-enter your choice!");		 
+					 	scn.nextLine();
+				 	}catch(MismatchException e) {
+				 		System.out.println("You Put the Wrong Number! please re-enter your choice!");
+				 		check=1;
+				 	}
+				 }
+						 
+				if(check==0) {
 					 System.out.println("Okay.\n");
 					 break;
 				 }else if(check==1)
 					 System.out.println("Please re-enter your name.");
-				 else
-					 System.out.println("You Typed Wrong Number!");
-			 }
-			 Character ch = new Character(name,hp); //set name, hp
+			}
+			 new Character(name,hp); //set name, hp
 			 System.out.println("Name : "+Character.getName()+"\nHp : "+Character.getHp());
-			 for(;1==check()||Character.getHp()>0;) {
+			 for(;1==check()&&Character.getHp()>0;) {
 				 System.out.println("\nWhere should we go?");
 				 System.out.println("0 : Save");
 				 for(int i=0;i<10;i++)
-					 if(clear[i]==0)
+					 if(clear[i]==false)
 						 System.out.println(i+1+" : "+Sname[i]);
 			    menu();
+			    
 				System.out.print("Select place: ");
-				menu=scn.nextInt();
-				//menu--;
+				while(check==0) {
+					try {
+					menu=scn.nextInt();
+					scn.nextLine();
+					if(menu<0||menu>10) throw new MismatchException ();
+					break;
+					}catch(InputMismatchException e){
+						System.out.println("You Put the Wrong Type!! please re-enter your choice!");		 
+					 	scn.nextLine();
+					}
+					catch(MismatchException e) {
+						System.out.println("You Put the Wrong Number! please re-enter your choice!");
+					}
+				}//menu--;
 				System.out.println(menu);
-				if(menu>9||menu<-1) {
-					System.out.println("You Typed Wrong Number!");
-					System.out.println("Please re-enter.");
-				}else if(clear[menu]==1) {
+				if(clear[menu]==true) {
 					System.out.println("It is Already Cleared!!");
 					System.out.println("Please re-enter.");
 				}
@@ -69,65 +91,64 @@ static String name;
                 		//SAVE
 						break;
 					case 1: //GLC
-						hangman h=new hangman();
-						h.game();
+						//hangman h=new hangman();
+					//	h.game();
 						break;
-					case 2: //Hyoam Chpel
-						Bible b=new Bible();
-						b.game();
+					case 2: 
+						//Hyoam Chpel
+						//Bible b=new Bible();
+					//	b.game();
 						break;
 					case 3: //Cheonmaji
-					 	Chunmaji.show();
+					 //	Chunmaji.show();
 						break;
 					case 4: //Store
-						Store.show();
+					//	Store.show();
 						break;
 					case 5: //Alpha Store
-					 	TicTac tic = new TicTac();
-						tic.tt();
-						ch.growHp(tic.reduceHp());
-						System.out.println("HP: " + ch.getHp());
-						System.out.println();
+					 //	TicTac tic = new TicTac();
+						//tic.tt();
+						//ch.growHp(tic.reduceHp());
+						//System.out.println("HP: " + ch.getHp());
+						//System.out.println();
 						break;
 					case 6: //Student Union
 						break;
 					case 7: //Hiddink Field
-					 	Predict predict = new Predict();
-						predict.pd();
-						ch.growHp(predict.reduceHp());
-						System.out.println("HP: " + ch.getHp());
-						System.out.println();
+					 	//Predict predict = new Predict();
+						//predict.pd();
+						//ch.growHp(predict.reduceHp());
+						//System.out.println("HP: " + ch.getHp());
+					//	System.out.println();
 						break;
-					case 8: //Oseok Hall
-						cgame c=new cgame();
-					    ch.growHp(-cgame.main(name));
-						break;
+					case 8:
+						TypingGame tg=new TypingGame();
+                		clear[menu]=tg.tgmain();
+ 						break;
 					case 9: //Newton Hall
-					 	//TypingGame tg=new TypingGame();
-                		//ch.growHp(-tg.main());
-						break;
+						cgame c=new cgame();
+					    clear[menu]=c.main();
+					    break;
 					case 10: //Hyundong Hall
-            			Hyeondong hd = new Hyeondong();
-						hd.show();
-						hd.quiz();
-            			ch.growHp(hd.getDamage());
-						System.out.println("Remaining Health: "+ch.getHp()+"\n");
+            		//	Hyeondong hd = new Hyeondong();
+						//hd.show();
+						//hd.quiz();
+            		//	ch.growHp(hd.getDamage());
+					//	System.out.println("Remaining Health: "+ch.getHp()+"\n");
 						break;
 				}
         
 			 }
 			 if(Character.getHp()<0) {
 				 System.out.println("Game is over");
-				 System.exit(0);
 			 }
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+			
+		
 	 }
+
 	 public	static int check() {
 		 for(int i=0;i<10;i++) {
-			 if(clear[i]==0)
+			 if(clear[i]==false)
 			 return 1;
 		 }
 		 return 0;
