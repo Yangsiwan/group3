7912f8dd8c649ff91cfc575d6f0326a1493d185c
MasicTurtle <61458692+HyeokyYun@users.noreply.github.com>
2020-06-07 20:27:44 +0900

    Update Predict.java

--- src/game/Predict.java
+++ src/game/Predict.java
@@ -1,121 +1,86 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
-import java.util.Scanner;
+package main;
 
-public class TicTac {
-	
-	//contain inputs in arraylist
-	private static ArrayList<Integer> playerPositions = new ArrayList<Integer>();
-	private static ArrayList<Integer> cashierPositions = new ArrayList<Integer>();
-	private static int lost = 0;
-	private static int played = 0;
+import java.util.*;
+
+public class Predict {
+	static boolean pass = true;
+	private static int correct = 0;
+	private static int start = 0;
 	private static int damage = 0;
+	private static int wrong = 0;
+	private static int played = 0;
 	
-	public static boolean tt() {
+	public boolean pd() {
+		Match game = new Match();
+		Scanner key = new Scanner(System.in);
 
-		try {
-			System.out.println("Cashier: Welcome to Alpha Store!");
-			Thread.sleep(1000);
-			System.out.println("Cashier: Here, we sell various office supplies.");
-			Thread.sleep(1000);
-			System.out.println("[" + Character.getName()+"]: Wait, what is this?");
-			Thread.sleep(1000);
-			System.out.println("Cashier: Ahhh, that is an old game called TicTacToe.");
-			Thread.sleep(1000);
-			System.out.println("[" + Character.getName()+"]: Can I buy this?");
-			Thread.sleep(1000);
-			System.out.println("Cashier: Well, that is not for sale.");
-			Thread.sleep(1000);
-			System.out.println("[" + Character.getName()+"]: But I want this game.");
-			Thread.sleep(1000);
-			System.out.println("Cashier: Hmm... Ok, but here is the deal,\n\t If you beat the game with me, \n\t I will give it to you for free.");
-			Thread.sleep(1000);
-			System.out.println("Cashier: Only winning is the key. No tie.");
-			System.out.println("[" + Character.getName()+"]: Got it!");
-			Thread.sleep(1000);
-			System.out.println("Cashier: Here is the game rules.");
-			Thread.sleep(1000);
-			System.out.println();
-			System.out.println("[Tic Tac Toe Rule]");
-			System.out.println("1. The game is played on a grid that's 3 squares by 3 squares.\n2. You are X, your friend is O. Players take turns putting their marks in empty squares.\n3. The first player to get 3 of her marks in a row (up, down, across, or diagonally) is the winner.\n4. When all 9 squares are full, the game is over. If no player has 3 marks in a row, the game ends in a tie.");
-			System.out.println("\nBasically, you need to make three in a row.");
-			Thread.sleep(1000);
-			System.out.println("\n[Example]");
-			System.out.print("X X X or ");
-			System.out.print("X or X\n         X      X\n         X        X");
-		}
-		catch (InterruptedException e) {
-			return false;
-		}
-			
-			//Game Board Form
-		char[][] gameBoard = {{' ', '|', ' ', '|', ' '},
-				{'-', '+', '-', '+', '-'},
-				{' ', '|', ' ', '|', ' '},
-				{'-', '+', '-', '+', '-'},
-				{' ', '|', ' ', '|', ' '}};
-			
-		System.out.println("\nGame Start!\n");
-		printGameBoard(gameBoard);
-			
-		//keeps asking to put new inputs
-		while(true) {
-			Scanner scan = new Scanner(System.in);
-			System.out.print("Enter your placement(1 - 9): ");
-			int playerPos = scan.nextInt();
-			
-			//To check if chosen place is already taken
-			while(playerPositions.contains(playerPos) || cashierPositions.contains(playerPos)) {
-				System.out.println("Position is already taken! Enter other positions");
-				playerPos = scan.nextInt();
-			}
-				
-			//call placeInput() method for player's inputs
-			placeInput(gameBoard, playerPos, "player");
-				
-			String result = checkWinner();
-			//if result's length is greater than 0, program ends;
-			if(result.length()>0) {
-				System.out.println(result);
-				System.out.println("Result");
-				printGameBoard(gameBoard);
+
+		//Game description
+		while(pass) {
+			try {
+				System.out.println("\nMove to Hiddink Field....\n");
+				Thread.sleep(1000);
+				System.out.println("Hi, [" + Character.getName() + "]!");
+				System.out.println("Welcome to Hiddink Field!!!");
+				Thread.sleep(1000);
+				System.out.println("Right now we have Handong Futsal League.");
+				System.out.println("You will watch three matches.");
+				System.out.println("You need to predict which team will win the match!");
+				Thread.sleep(1000);
+				System.out.println("You can pass this stage, if your prediction corrects more than 2!");
+				System.out.println("Trust your gut!");
+				Thread.sleep(1000);
 				System.out.println();
+				System.out.println("If you are ready, press 1.");
+				System.out.println("If you want to leave, press 0.");
+			}
+			catch(InterruptedException e) {
+				return false;
+			}
+			while(true){
+				//Get users' opinion.
+				System.out.print("Your Choice: ");
+				start = key.nextInt();
+				
+				if(start > 1 || start < 0){
+					System.out.println("\nWrong input!");
+				}
+				if(start == 1 || start == 0)break;
+			}
+
+			//If users choose 0, quit game.
+			if(start == 0){
 				break;
 			}
-				
-			//generates random numbers for inputs of Cashier
-			Random rand = new Random();
-			int cashPos = rand.nextInt(9) + 1;
-			//To check if chosen place is already taken and generates ew numbers to input.
-			while(playerPositions.contains(cashPos) || cashierPositions.contains(cashPos)) {
-				cashPos = rand.nextInt(9) + 1;
+			//If users choose 1, It calls Game class
+			if(start == 1) {
+				System.out.println("Good Choice!");
+				if(game.Game1()) correct++;
+				else wrong++;
+				if(game.Game2()) correct++;
+				else wrong++;
+				if(game.Game3()) correct++;
+				else wrong++;
 			}
-			placeInput(gameBoard, cashPos, "Cashier");
-				
-			//call printGameBoard() after set place of 'X'
-			System.out.println();
-			printGameBoard(gameBoard);
 			
-			//call checkWinner() method and print winning message
-			result = checkWinner();
-			if(result.length()>0) {
-				System.out.println(result);
-				System.out.println();
+			if(start == 1) {
+				//Shows users scores.
 				System.out.println("Result");
-				printGameBoard(gameBoard);
-				System.out.println();
-				break;
+				System.out.println("Correct: " + correct + " Wrong: " + wrong);
+				pass = false;
+			}
+			
+			if((correct == 0 && wrong == 0) || (correct == 3 && wrong == 0) || (correct == 2 && wrong == 1) || (correct == 1 && wrong == 2) || (correct == 0 && wrong == 3)) {
+				pass = false;
 			}
 		}
-		
-		if(lost == 0) {
+
+		if(start == 1 && correct > 1) {
 			damage = 0;
 			Character.growHp(-damage);
 			return true;
 		}
-		else if(lost == 1){
+		else if(start == 1 && correct < 2){
 			if(played == 0){
 				damage = 5;
 				Character.growHp(-damage);
@@ -125,106 +90,5 @@ public class TicTac {
 		}
 		return false;
 	}
-	
-	//Print out gameBoard() method
-	public static void printGameBoard(char[][] gameBoard) {
-
-		for(char[] row : gameBoard) {
-			for(char c : row) {
-				System.out.print(c);
-			}
-			System.out.println();
-		}
-	}
-	
-	//Place inputs from users
-	public static void placeInput(char[][] gameBoard, int pos, String user) {
-		
-		char symbol = ' ';
-		
-		//Different inputs from player and cashier
-		if(user.equals("player")) {
-			symbol = 'X';
-			playerPositions.add(pos);
-		}
-		else if(user.contentEquals("Cashier")) {
-			symbol = 'O';
-			cashierPositions.add(pos);
-		}
-		
-		//Put 'X' or 'O' on right place on the game board.
-		switch(pos) {
-			case 1:
-				gameBoard[0][0] = symbol;
-				break;
-			case 2:
-				gameBoard[0][2] = symbol;
-				break;
-			case 3:
-				gameBoard[0][4] = symbol;
-				break;
-			case 4:
-				gameBoard[2][0] = symbol;
-				break;
-			case 5:
-				gameBoard[2][2] = symbol;
-				break;
-			case 6:
-				gameBoard[2][4] = symbol;
-				break;
-			case 7:
-				gameBoard[4][0] = symbol;
-				break;
-			case 8:
-				gameBoard[4][2] = symbol;
-				break;
-			case 9:
-				gameBoard[4][4] = symbol;
-				break;
-			default:
-				break;
-		}
-	}
-	
-	//Winning condition method
-	public static String checkWinner() {
-			
-		//winning positions check that users' inputs contain these conditions
-		List topRow = Arrays.asList(1, 2, 3);
-		List midRow = Arrays.asList(4, 5, 6);
-		List botRow = Arrays.asList(7, 8, 9);
-		List leftCol = Arrays.asList(1, 4, 7);
-		List midCol = Arrays.asList(2, 5, 8);
-		List rightCol = Arrays.asList(3, 6, 9);
-		List cross1 = Arrays.asList(1, 5, 9);
-		List cross2 = Arrays.asList(3, 5, 7);
-		
-		//contains winning condition in List of List.
-		List<List> winningConditions = new ArrayList<List>();
-		winningConditions.add(topRow);
-		winningConditions.add(midRow);
-		winningConditions.add(botRow);
-		winningConditions.add(leftCol);
-		winningConditions.add(midCol);
-		winningConditions.add(rightCol);
-		winningConditions.add(cross1);
-		winningConditions.add(cross2);
-		
-		//read all the contents of List
-		for(List l : winningConditions) {
-			//when player fulfills winning condition
-			if(playerPositions.containsAll(l)) { 
-				return "Congratulations you won!";
-			}
-			else if(cashierPositions.containsAll(l)) {
-				lost++;
-				return "I won! Sorry.";
-			}
-			else if((!playerPositions.containsAll(l) && !cashierPositions.containsAll(l)) && (playerPositions.size() + cashierPositions.size() == 9)) {
-				lost++;
-				return "DRAW!";
-			}
-		}	
-		return "";		
-	}
 }
+
